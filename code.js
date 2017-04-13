c45c90246a2950bb27cc14e06af5960b37974444
David Waller <david@krawaller.se>
2017-04-13 13:04:16 +0200

    initial commit

---
+++ code.js
@@ -0,0 +1,18 @@
+var fs = require("fs");
+var babel = require("babel-core");
+var opts = JSON.parse(fs.readFileSync(__dirname+'/.babelrc')+'');
+
+
+var filename = process.argv[2].replace(/\.js$/,'');
+
+console.log("Reading file", filename);
+var code = fs.readFileSync(__dirname+'/code/'+filename + '.js')+'';
+
+console.log("Transpiling", filename);
+var result = babel.transform(code, opts);
+
+console.log('Writing transpiled code to /code/'+filename+'.transpilation.js');
+fs.writeFile(__dirname+'/code/'+filename+'.transpilation.js', result.code);
+
+console.log("Executing transpiled code:");
+eval(result.code);
---
+++ code/simpletest.js
@@ -0,0 +1,4 @@
+let name = 'test';
+let obj = {name};
+
+[1,2,3].forEach(num => console.log('number',num));
